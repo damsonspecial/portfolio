3614c93fc9a6448d5d24c9cccda02c93fbf52f59
atiaro damilola <42834561+damsonspecial@users.noreply.github.com>
2024-11-01 11:42:28 +0100

    my third commit

--- config/database.php
+++ config/database.php
@@ -60,8 +60,20 @@ return [
             'options' => extension_loaded('pdo_mysql') ? array_filter([
                 PDO::MYSQL_ATTR_SSL_CA => env('MYSQL_ATTR_SSL_CA'),
             ]) : [],
+        ],      
+        'libsql' => [
+            'driver' => 'libsql',
+            'url' => env('DB_DATABASE', database_path('database.sqlite')),
+            'authToken' => env('DB_AUTH_TOKEN', ''),
+            'syncUrl' => env('DB_SYNC_URL', ''),
+            'syncInterval' => env('DB_SYNC_INTERVAL', 5),
+            'read_your_writes' => env('DB_READ_YOUR_WRITES', true),
+            'encryptionKey' => env('DB_ENCRYPTION_KEY', ''),
+            'remoteOnly' => env('DB_REMOTE_ONLY', false),
+            'database' => null,
+            'prefix' => '',
         ],
-
+        
         'mariadb' => [
             'driver' => 'mariadb',
             'url' => env('DB_URL'),
